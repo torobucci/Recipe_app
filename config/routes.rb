9c312ccc06a7e84b03e8c61a68790a5d523585a7
torobucci <kevintoro254@gmail.com>
2023-07-04 11:41:28 +0300

    configure database and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'recipes#index'
+  resources :recipes, only: %i[index show destroy]
 end
