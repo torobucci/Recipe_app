a593ec1e0e6004ad04e5c30d3268cc892e770aeb
torobucci <kevintoro254@gmail.com>
2023-07-05 23:27:11 +0300

    add unit and integration specs for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,10 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :public, inclusion: { in: [true, false] }
 end
