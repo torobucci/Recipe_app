8a960c0d0188e24b565e222455d19e9315e6a94e
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-07-06 18:56:39 +0100

    Style for shopping list

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,7 @@
 class FoodsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @foods = current_user.foods
+    @foods = current_user.foods.includes(:user)
   end
 
   def new
